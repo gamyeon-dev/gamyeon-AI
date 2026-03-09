7d3b29c64b4e0ee3697b9a46a7ce822c03249933
JaeWoong Eom <zzang6535@gmail.com>
2026-03-09 17:09:21 +0900

    chore: Agent-server consul 설정, CI/CD 설정 테스트
    - consul server에 붙기 위한 설정
    - github action로 CI/CD 테스트

--- app/main.py
+++ app/main.py
@@ -1,12 +1,38 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 from dotenv import load_dotenv
+import consul, os
 
 load_dotenv()
+
+# Configuration about consul client
+consul_host = os.getenv("CONSUL_HOST", "localhost")
+c = consul.Consul(host = consul_host, port = 8500)
+SERVICE_ID = "AGENT-SERVER"
+EXTERNAL_HOST_IP = "172.17.0.1"
+EC2_PUBLIC_IP = "15.164.229.233"
+
+@asynccontextmanager
+async def lifespan(app: FastAPI) :
+    c.agent.service.register(
+        name = "agentapi-app",
+        service_id = SERVICE_ID,
+        address = EXTERNAL_HOST_IP,
+        port = 8500,
+        check = consul.Check.http(f"http://{EC2_PUBLIC_IP}:8000/health", interval = "10s")
+    )
+    print("Consul 등록 완료")
+
+    yield # 서버 실행
+
+    c.agent.service.deregister(SERVICE_ID)
+    print("Consul 등록 해제")
 
 app = FastAPI(
     title="Interview AI Server",
     description="AI Interview Simulator - AI Features",
-    version="0.1.0"
+    version="0.1.0",
+    lifespan = lifespan
 )
 
 @app.get("/health")
