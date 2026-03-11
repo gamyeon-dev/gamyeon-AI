849b212696391b30c26c641c30db3d65c5800f10
JaeWoong Eom <zzang6535@gmail.com>
2026-03-11 09:04:23 +0900

    Agent-server 인프라 설정 (#3)
    * chore: Agent-server consul 설정, CI/CD 설정 테스트
    
    - consul server에 붙기 위한 설정
    - github action로 CI/CD 테스트
    
    * chore: Agent-server consul 설정, CI/CD 설정 테스트 2
    
    - Docker 관련 구성
    
    * chore: Agent-server consul 설정, CI/CD 설정 테스트 3
    
    - Dockerfile 관련 구성 uv 인식하도록 수정
    
    * chore: Agent-server consul 설정, CI/CD 설정 테스트 4
    
    Dockerfile -> 공식 환경에서 패키지 설치 후 배포
    
    * chore: Agent-server consul 설정, CI/CD 설정 테스트 4
    
    Dockerfile -> CMD 경로 수정
    
    * chore: Agent-server consul 설정, CI/CD 설정 테스트 6
    
    Dockerfile -> uv 공식 이미지 가져와 설치
    
    * chore: Agent-server consul 설정, CI/CD 설정 테스트 7
    
    - main.py address, port 수정
    
    * chore: Agent-server consul 설정, CI/CD 설정 테스트 8
    
    main.py name 변경
    
    * chore: Agent-server consul 설정, CI/CD 설정 테스트 9
    
    - main.py consul key/value 사용
    
    * chore: Agent-server consul 설정, CI/CD 설정 테스트 10
    
    main.py consul key/value 사용 방법 변경
    
    * chore: Agent-server consul 설정, CI/CD 설정 테스트 11
    
    main.py consul key/value 사용 방법 변경(대소문자)
    
    * chore: Agent-server consul 설정, CI/CD 설정 테스트 12
    
    main.py consul key/value 사용 방법 변경 - 일단 일반 문자열로 변경
    
    * chore: Agent-server consul 설정, CI/CD 설정 테스트 13
    
    * chore: Agent-server consul 설정, CI/CD 설정 테스트 13
    
    - key 기반의 json값을 받아와서 파싱하는 구조 생성
    
    * chore: Agent-server consul 설정, CI/CD 설정 테스트 14
    
    * chore: Agent-server consul 설정, CI/CD 설정 테스트 15
    
    * chore: Agent-server consul 설정, CI/CD 설정 테스트 16
    
    * chore: Agent-server consul 설정, CI/CD 설정 테스트 17
    
    * chore: Agent-server consul 설정, CI/CD 설정
    
    - consul server에 붙기 위한 설정
    - github action을 이용한 CI/CD 구축
    - Docker 관련 구성
    
    * chore: Agent-server consul 설정, CI/CD 설정 18
    
    - spring 서버 변경에 따른 수정
    
    * chore: Agent-server consul 설정, CI/CD 설정 19
    
    * chore: Agent-server consul 설정, CI/CD 설정 19
    
    * chore: Agent-server consul 설정, CI/CD 설정 20
    
    * chore: Agent-server consul 설정, CI/CD 설정
    
    * chore: Agent-server consul 설정, CI/CD 설정

--- app/main.py
+++ app/main.py
@@ -1,14 +1,51 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from app.core import ConsulHelper
 from dotenv import load_dotenv
+import consul, os, uuid
 
 load_dotenv()
+
+# Configuration about consul client
+consul_host = os.getenv("CONSUL_HOST", "localhost")
+c = consul.Consul(host = consul_host, port = 8500)
+
+@asynccontextmanager
+async def lifespan(app: FastAPI) :
+    consul_helper = ConsulHelper(host="consul")
+    config = consul_helper.get_config("config/agent/settings")
+
+    SERVICE_ID = config.get("SERVICE_ID", "DEFAULT-SERVER")
+    EXTERNAL_HOST_IP = config.get("EXTERNAL_HOST_IP", "127.0.0.1")
+    EC2_PUBLIC_IP = config.get("EC2_PUBLIC_IP", "0.0.0.0")
+
+    # UUID를 사용하여 매번 다른 ID 생성
+    unique_id = f"{SERVICE_ID}:{uuid.uuid4()}" 
+
+    c.agent.service.register(
+        name = SERVICE_ID,
+        service_id = unique_id,
+        address = EXTERNAL_HOST_IP,
+        port = 8000,
+        check = consul.Check.http(f"http://{EC2_PUBLIC_IP}:8000/health", interval = "10s")
+    )
+    print("Consul 등록 완료")
+
+    yield # 서버 실행
+
+    c.agent.service.deregister(SERVICE_ID)
+    print("Consul 등록 해제")
 
 app = FastAPI(
     title="Interview AI Server",
     description="AI Interview Simulator - AI Features",
-    version="0.1.0"
+    version="0.1.0",
+    lifespan = lifespan
 )
 
 @app.get("/health")
 def health_check():
-    return {"status": "ok", "message": "AI server is running"}
+    return {
+        "status": "ok", 
+        "message": "AI server is running"
+    }
