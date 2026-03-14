efb82d96fa253c8c4832e05250b11d5112c46c82
Seohyun Kim <102886338+betterb2etr@users.noreply.github.com>
2026-03-14 15:18:22 +0900

    fix: fix domain according erd

--- app/feedback/schema/response.py
+++ app/feedback/schema/response.py
@@ -1,7 +1,7 @@
 from pydantic import BaseModel
 from typing import Optional
 
-
+ # ! reliability
 class FeedbackResponse(BaseModel):
     intv_question_id:          int
     status:                    str
